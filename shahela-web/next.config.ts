1cef6c0e55b622fe9df872352d36f6581b9825db
Fahim-hash <82995127+Fahim-hash@users.noreply.github.com>
2026-04-13 21:30:45 +0600

    Delete shahela-web directory

--- shahela-web/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // @ts-ignore - experimental ব্লক ছাড়া সরাসরি ব্যবহারের চেষ্টা
-  allowedDevOrigins: ['192.168.0.102'],
-};
-
-export default nextConfig;
