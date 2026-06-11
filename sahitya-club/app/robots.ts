68496a619712d4509f854e8553a8434c816e386a
Syed Fahim Muddasir <82995127+Fahim-hash@users.noreply.github.com>
2026-06-11 13:05:14 +0600

    Update robots.ts

--- sahitya-club/app/robots.ts
+++ sahitya-club/app/robots.ts
@@ -1,13 +1,12 @@
 // app/robots.ts
-import { MetadataRoute } from 'next';
-
+import { MetadataRoute } from 'next'
+ 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/private/', '/admin/'], // যদি কোনো গোপন বা প্যানেল পেজ ক্রল করাতে না চাও
     },
-    sitemap: 'https://wlc.pro.bd', // তোমার লাইভ ইউআরএলটি এখানে দেবে
-  };
+    sitemap: 'https://wlc.pro.bd/sitemap.xml',
+  }
 }
