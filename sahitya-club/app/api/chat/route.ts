2b7f4479159db3cad16735f691f138c07b1db17b
Syed Fahim Muddasir <82995127+Fahim-hash@users.noreply.github.com>
2026-09-23 18:45:17 +0600

    Update route.ts

--- sahitya-club/app/api/chat/route.ts
+++ sahitya-club/app/api/chat/route.ts
@@ -48,7 +48,7 @@ export async function POST(req: Request) {
 
     const completion = await groq.chat.completions.create({
       messages: [systemPrompt, ...messages],
-      model: "llama-3.3-70b-versatile",
+     model: "openai/gpt-oss-120b",
       temperature: 0.5,
       max_tokens: 1024,
     });
